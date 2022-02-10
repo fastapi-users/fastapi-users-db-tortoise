99f4bfab1c5fd53df5b8c1cba7c1f2707161462f
François Voron <fvoron@gmail.com>
2022-02-10 11:29:48 +0100

    Fix warnings regarding asyncio

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ class UserDBOAuth(UserOAuth, UserDB):
 @pytest.fixture(scope="session")
 def event_loop():
     """Force the pytest-asyncio loop to be the main one."""
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     yield loop
 
 
